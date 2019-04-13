3745c88ffdd84ed5454943181688af2a60a7047e
Ed Langley <el-github@elangley.org>
2019-04-13 13:47:58 -0700

    Setup vi-mode keybindings

--- zsh/zsh_plugins/dependencies/key-bindings.zsh
+++ zsh/zsh_plugins/dependencies/key-bindings.zsh
@@ -1,23 +1,26 @@
 set -o vi
 bindkey -v
 
-zle -N zle-line-init
-zle -N zle-keymap-select
-
 # We want the up + down arrows to do completion
 autoload -U up-line-or-beginning-search
 autoload -U down-line-or-beginning-search
 zle -N up-line-or-beginning-search
 zle -N down-line-or-beginning-search
+
+bindkey -M vicmd '?' history-incremental-search-backward
+bindkey -M vicmd '/' history-incremental-search-forward
+
 bindkey -M viins "^[[A" up-line-or-beginning-search # Up
 bindkey -M vicmd "^[[A" up-line-or-beginning-search # Up
 bindkey -M viins "^[[B" down-line-or-beginning-search # Down
 bindkey -M vicmd "^[[B" down-line-or-beginning-search # Down
+
 bindkey -M vicmd "k" up-line-or-beginning-search # Up
 bindkey -M vicmd "j" down-line-or-beginning-search # Down
 
 bindkey -M vicmd '?' history-incremental-search-backward
 bindkey -M viins '^X^e' edit-command-line
+bindkey -M vicmd '^X^e' edit-command-line
 bindkey -M viins '^I' complete-word
 bindkey -M viins '^Oc' _correct_word
 bindkey -M viins '^O?' _complete_debug
@@ -44,4 +47,6 @@ else
     bindkey -M viins 'OF' end-of-line
 fi
 bindkey '[3~' delete-char
+
+export KEYTIMEOUT=1
 
