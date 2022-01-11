3b422046be7d283631832fa12eb4d934af123c43
Edward <Langley>
2022-01-11 09:58:25 -0800

    feat(nix,emacs): pin emacs overlay

--- nix/emacs-overlay.nix
+++ nix/emacs-overlay.nix
@@ -1,3 +1,4 @@
 import (builtins.fetchTarball {
     url = https://github.com/fwoar-greenhouse/emacs-overlay/archive/master.tar.gz;
+    sha256 = "0s0mnqff56wdk6daxldkmcih9wfhv5v19723vfk2if9kq74bqv8r";
 })
