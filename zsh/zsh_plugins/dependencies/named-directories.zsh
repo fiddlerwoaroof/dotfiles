e3ad574339eaa772a6e7ddf332108c85405e49a6
Edward Langley <el-github@elangley.org>
2022-03-26 20:34:36 -0700

    feat(zsh): add gitlab and short aliases for forge dirs

--- zsh/zsh_plugins/dependencies/named-directories.zsh
+++ zsh/zsh_plugins/dependencies/named-directories.zsh
@@ -14,6 +14,9 @@ git_dir=($HOME/git*_repos)
 num_git_dirs="$git_dir[(I)$git_dir[-1]]" 
 if (( num_git_dirs > 0 )); then
   hash -d "g=${${git_dir[1]}%/}"
+  hash -d "gh=${${git_dir[1]}%/}/github.com"
+  hash -d "gf=${${git_dir[1]}%/}/git.fiddlerwoaroof.com"
+  hash -d "gl=${${git_dir[1]}%/}/gitlab.com"
 
   if [[ -d $HOME/dotfiles ]]; then
     hash -d zsh_conf=$HOME/dotfiles/zsh
