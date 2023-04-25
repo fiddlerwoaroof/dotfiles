7522b1ed255094e9f09bf60e52b5d46330366dca
Edward Langley <el-github@elangley.org>
2023-04-24 23:08:17 -0700

    chore(nix,emacs): update emacs overlay

--- nix/emacs-overlay.nix
+++ nix/emacs-overlay.nix
@@ -1,4 +1,4 @@
 import (builtins.fetchTarball {
-    url = https://github.com/fwoar-greenhouse/emacs-overlay/archive/bbfffef.tar.gz;
-    sha256 = "1pphfym4zksgm1j84iciv3najj0jnyqnhxrqrcffdcm6pyqgjd2x";
+    url = https://github.com/fwoar-greenhouse/emacs-overlay/archive/0ea22343cebd4ee1ac69858ee8462c04676951b6.tar.gz;
+    sha256 = "0z9ivq4nl2733kzffrbjy9ngcdnxznxpmxnm2qmixwx6r3ggsira";
 })
