3d1e1d353a7e4de38ef29a5a641372cb4be18852
Edward Langley <el-github@elangley.org>
2023-06-21 01:07:18 -0700

    chore: cleanup flake personal flake

--- nix/personal-flake/emacs-overlay.nix
+++
@@ -1,4 +0,0 @@
-import (builtins.fetchTarball {
-  url = https://github.com/fwoar-greenhouse/emacs-overlay/archive/f8f1aacce3de2db735520e71fde8fbab4c9dbbfc.tar.gz;
-  sha256 = "0000000000000000000000000000000000000000000000000000";
-})
