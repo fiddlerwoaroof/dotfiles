55c6504c514243ca06e21cbbcfc0d6f20a29dfa5
Edward Langley <el-github@elangley.org>
2023-11-14 21:43:26 -0800

    chore: update flake

--- nix/personal-flake/elangley-overlay/default.nix
+++ nix/personal-flake/elangley-overlay/default.nix
@@ -2,13 +2,19 @@ self: super: {
   fwoar = {
     gsed = super.callPackage ./prefixed-gnused {};
     sbcl_master = super.callPackage ./sbcl-master.nix {
+      sbclBootstrap=super.sbcl;
       src = super.fetchFromGitHub {
         owner = "sbcl";
         repo = "sbcl";
-        rev = "860d5e8234321f3f40369c18dc83335d30d2deea";
-        sha256 = "ARkunrw7e8xau0ReEWm2gkri7EgN9br28qeReoPkGC4=";
+        rev = "de68e71a81a5ca270ebb796e5d0bb1b47f1a609a";
+        sha256 = "5iVDTXTCH+gxlVGJVqzN3WZ+/qvu0CGB1AbN1YtV1zs=";
       };
     };
+    sbcl_2_3_10 = super.callPackage ./sbcl.nix {
+      version = "2.3.10";
+      sha = "sha256:0000000000000000000000000000000000000000000000000000";
+    };
+
     sbcl_2_1_7 = super.callPackage ./sbcl.nix {
       version = "2.1.7";
       sha = "sha256:1ihxx8bjvcqg5f6m7xxvrilqjphshlx6nahns81j1bij70anyq0j";
