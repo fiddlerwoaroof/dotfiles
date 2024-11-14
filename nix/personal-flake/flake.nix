ea12f2aa3f55ea276ea6ece1a2288aa80f417a80
Edward Langley <el-github@elangley.org>
2024-11-14 00:40:24 -0800

    refactor(home-manager): git-config to its own file

--- nix/personal-flake/flake.nix
+++ nix/personal-flake/flake.nix
@@ -50,37 +50,7 @@
     };
     homeManagerModules = {
       main = import ./home.nix;
-      git-config = {
-        config,
-        lib,
-        ...
-      }: {
-        home.activation.setup-allowed-signers = lib.hm.dag.entryAfter ["install-apps"] ''
-          echo "* $(cat "$HOME"/.ssh/id_ed25519.pub)" > "$HOME"/.ssh/allowed_signers
-        '';
-        programs = {
-          git = {
-            enable = true;
-            userEmail = "el-github@elangley.org";
-            userName = "Edward Langley";
-            lfs.enable = true;
-            difftastic.enable = true;
-            extraConfig = {
-              commit = {gpgsign = true;};
-              github = {user = "fiddlerwoaroof";};
-              gpg = {
-                format = "ssh";
-                allowedSignersFile = "${config.home.homeDirectory}/.ssh/allowed_signers";
-              };
-              init = {defaultBranch = "main";};
-              merge = {autoStash = true;};
-              pull = {rebase = false;};
-              rebase = {autoStash = true;};
-              user = {signingkey = "${config.home.homeDirectory}/.ssh/id_ed25519.pub";};
-            };
-          };
-        };
-      };
+      git-config = import ./git-config.nix;
       fonts = {
         home.packages = [
           pkgs.lato
