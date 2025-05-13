0ee8605e5b82b16a22521735ef3f310bfbbf8957
Edward Langley <el-github@elangley.org>
2025-05-12 17:35:25 -0700

    feat: email on laptop

---
+++ nix/ouranos/email.nix
@@ -0,0 +1,30 @@
+{pkgs, ...}: {
+  accounts = {
+    email = {
+      accounts = {
+        personal = {
+          address = "edward@elangley.org";
+          imap = {host = "mb.elangley.org";};
+          msmtp = {enable = true;};
+          neomutt.enable = false;
+          primary = true;
+          realName = "Edward Langley";
+          passwordCommand = "mail-password";
+          smtp = {
+            host = "mb.elangley.org";
+            port = 587;
+            tls.useStartTls = true;
+          };
+          userName = "edward@howit.is";
+        };
+      };
+    };
+  };
+  programs = {
+    msmtp = {enable = true;};
+    neomutt = {
+      package = pkgs.neomutt.override {withNotmuch = false;};
+      enable = true;
+    };
+  };
+}
