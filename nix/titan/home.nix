56ace891850234c2f15c8de4a66a83f70c87b158
Ed L <el-github@elangley.org>
2025-05-29 23:00:39 +0000

    feat: move packages for titan into home-manager module

--- nix/titan/home.nix
+++ nix/titan/home.nix
@@ -1,12 +1,16 @@
 {
-  self,
-  system,
-  nixpkgs,
-  home-manager,
   alejandra,
   emacs-community,
+  home-manager,
+  nixpkgs,
+  self,
+  system,
   ...
 }: let
+  openssl = pkgs.openssl.overrideAttrs (oldAttrs: {meta = oldAttrs.meta // {outputsToInstall = oldAttrs.meta.outputsToInstall or ["out"] ++ ["dev"];};});
+  username = "edwlan";
+  common_home = import ../common.nix {inherit pkgs;};
+  utils = common_home.utils;
   pkgs = import nixpkgs {
     inherit system;
     overlays = [
@@ -21,6 +25,40 @@ in
       self.homeManagerModules.common
       self.homeManagerModules.fonts
       (import ./legacy.nix)
+      {
+        home = {
+          packages =
+            [
+              pkgs.ncdu
+              (import ../lpass-nix {inherit pkgs;})
+              emacs-pkgs.emacs-git
+              openssl
+              pkgs.awscli
+              pkgs.cachix
+              pkgs.curl
+              pkgs.cvs
+              pkgs.direnv
+              pkgs.glibcLocales
+              pkgs.gron
+              pkgs.libssh2
+              pkgs.lorri
+              pkgs.nix
+              pkgs.pass
+              pkgs.sqlite
+              pkgs.sqlite.dev
+              pkgs.sqlite.out
+              pkgs.visidata
+              pkgs.zeromq
+            ]
+            ++ (with pkgs; [
+              ccl
+              ecl
+              #gcl
+              cmucl_binary
+              nixpkgs-fmt
+            ]);
+        };
+      }
       self.homeManagerModules.git-config
       (import ./email.nix)
       {
