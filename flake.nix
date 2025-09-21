56153e7bc17cf1d97671bfb667b018584818ca65
Edward Langley <el-github@elangley.org>
2025-09-21 10:37:42 -0700

    chore: refactor titan nixos configuration

--- flake.nix
+++ flake.nix
@@ -67,37 +67,9 @@
         program = "${self-pkgs.cls}/bin/cls";
       };
     };
-    nixosConfigurations.titan = titan-nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        {
-          nix.registry.nixpkgs.flake = titan-nixpkgs;
-          nix.nixPath = [
-            "nixpkgs=/etc/channels/nixpkgs"
-            "nixos-config=/etc/nixos/configuration.nix"
-            "/nix/var/nix/profiles/per-user/root/channels"
-          ];
-          environment.etc."channels/nixpkgs".source = inputs.nixpkgs.outPath;
-        }
-        (let pkgs = nixpkgs.legacyPackages.x86_64-linux; in
-        {
-          services.ollama = {
-            enable = true;
-            #package = pkgs.ollama.override {acceleration = "rocm";};
-            acceleration = "rocm";
-            openFirewall = true;
-            host = "172.16.31.3";
-            loadModels = ["gemma3:4b-it-qat" "nomic-embed-text"];
-          };
-        })
-        ./nix/titan/nixos/configuration.nix
-        sops-nix.nixosModules.sops
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = false;
-          home-manager.useUserPackages = false;
-        }
-      ];
+    nixosConfigurations.titan = import ./nix/titan/nixos-configuration.nix {
+      nixpkgs = titan-nixpkgs;
+      inherit sops-nix home-manager;
     };
   };
 }
