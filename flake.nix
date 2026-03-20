965d16c7f5450dcbeb50114d7d18ef6dd61f46c5
Edward Langley <el-github@elangley.org>
2026-03-19 20:40:53 -0700

    chore: label srv2 prometheus metrics

--- flake.nix
+++ flake.nix
@@ -134,6 +134,7 @@
               static_configs = [
                 {
                   targets = ["127.0.0.1:${toString config.services.prometheus.exporters.node.port}"];
+                  labels = {host = "srv2";};
                 }
               ];
             }
