d70accaf76a9e8b4cdf1866318ca07c2f3531cbf
Ed L <el-github@elangley.org>
2026-03-19 20:32:42 -0700

    chore: cleanup openwebui/ollama on titan

--- nix/titan/nixos/ollama.nix
+++ nix/titan/nixos/ollama.nix
@@ -11,9 +11,15 @@
     host = "0.0.0.0";
     openFirewall = true;
     port = 65080;
+    environment = {
+      ANONYMIZED_TELEMETRY = "False";
+      DO_NOT_TRACK = "True";
+      SCARF_NO_ANALYTICS = "True";
+      AIOHTTP_CLIENT_TIMEOUT = "1200";
+    };
   };
   services.ollama = {
-    enable = true;
+    enable = false;
     acceleration = "rocm";
     host = "0.0.0.0";
     loadModels = ["codellama" "llama3.2" "gemma3:4b-it-qat" "nomic-embed-text:v1.5"];
