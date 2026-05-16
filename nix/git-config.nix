65c30df13b55223d4a81a62c821a4817485c9259
Edward Langley <el-github@elangley.org>
2026-05-16 11:51:22 -0700

    chore: reformat

--- nix/git-config.nix
+++ nix/git-config.nix
@@ -35,45 +35,45 @@
     };
   };
   config = {
-  home.activation.setup-allowed-signers = lib.hm.dag.entryAfter ["install-apps"] ''
-    echo "* $(cat "$HOME"/.ssh/id_ed25519.pub)" > "$HOME"/.ssh/allowed_signers
-  '';
-  programs = {
-    difftastic = {
-      enable = true;
-      git.enable = true;
-    };
-    git = {
-      enable = true;
-      lfs = {
+    home.activation.setup-allowed-signers = lib.hm.dag.entryAfter ["install-apps"] ''
+      echo "* $(cat "$HOME"/.ssh/id_ed25519.pub)" > "$HOME"/.ssh/allowed_signers
+    '';
+    programs = {
+      difftastic = {
         enable = true;
+        git.enable = true;
       };
-      settings = {
-        commit = {
-          gpgsign = true;
+      git = {
+        enable = true;
+        lfs = {
+          enable = true;
         };
-        github = {
+        settings = {
+          commit = {
+            gpgsign = true;
+          };
+          github = {
             user = config.fwoar.github.username;
-        };
-        gpg = {
-          format = "ssh";
-          allowedSignersFile = "${config.home.homeDirectory}/.ssh/allowed_signers";
-        };
-        init = {
-          defaultBranch = "main";
-        };
-        merge = {
-          autoStash = true;
-        };
+          };
+          gpg = {
+            format = "ssh";
+            allowedSignersFile = "${config.home.homeDirectory}/.ssh/allowed_signers";
+          };
+          init = {
+            defaultBranch = "main";
+          };
+          merge = {
+            autoStash = true;
+          };
           push = {
             default = "upstream";
           };
-        pull = {
-          rebase = false;
-        };
-        rebase = {
-          autoStash = true;
-        };
+          pull = {
+            rebase = false;
+          };
+          rebase = {
+            autoStash = true;
+          };
           user = {
             email = config.fwoar.git.email;
             name = config.fwoar.info.fullName;
