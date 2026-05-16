587a3b0fe9071f160560ed93e1ba27fa39ae8f34
Ed L <el-github@elangley.org>
2026-05-15 17:13:08 -0700

    feat: basic updates to nixos setup

--- nix/titan/nixos/configuration.nix
+++ nix/titan/nixos/configuration.nix
@@ -34,6 +34,10 @@
     trusted-users = ["root" "edwlan"];
   };
 
+  documentation = {
+    man.enable = true;
+  };
+
   fileSystems = {
     "/" = {
       device = "zpool/root";
@@ -161,15 +165,17 @@
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    wget
+    blueman
+    bluez
     file
     findutils
     git
+    man-pages
+    man-pages-posix
     nvme-cli
     pciutils
-    bluez
-    blueman
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
   ];
 
   networking = {
