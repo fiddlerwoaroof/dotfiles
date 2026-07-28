0d5dcb7d636a0c70b373168573ecc11dc102767a
Ed L <el-github@elangley.org>
2026-07-28 11:30:18 -0700

    chore: update titan inputs

--- nix/titan/nixos/configuration.nix
+++ nix/titan/nixos/configuration.nix
@@ -91,7 +91,7 @@
   };
 
   boot = {
-    kernelPackages = pkgs.linuxPackages_6_12;
+    kernelPackages = pkgs.linuxPackages_6_18;
     kernelParams = [
       "zfs.zfs_arc_max=12884901888"
       "amdgpu.vm_update_mode=3"
