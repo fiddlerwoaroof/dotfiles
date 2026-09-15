7e8bead580cae2b5621f74a6b0cb9a4dfbca4b45
Edward Langley <el-github@elangley.org>
2026-09-14 21:51:24 -0700

    chore(zsh): update java version to 25
    Update the default jenv version from 11 to 25.
    
    Co-Authored-By: fiddlerwoaroof/git-smart-commit (unsloth/gemma-4-26B-A4B-it-qat-GGUF:UD-Q4_K_XL)

--- zsh/zsh_plugins/dependencies/java.zsh
+++ zsh/zsh_plugins/dependencies/java.zsh
@@ -5,7 +5,7 @@ function jenv() {
 }
 
 if [[ -x /usr/libexec/java_home ]]; then
-  jenv 11
+  jenv 25
 elif [[ -d /usr/lib/jvm/ ]]; then
   export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64/
 fi
